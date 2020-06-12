5d00147cb87fbbd5f41a877a5092517f4e8acb42
Michika <renesmee0420@gmail.com>
2020-06-13 00:40:46 +0900

    gitignore

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
 
-DEV_ENV_LABEL = "twittersimple"
 
-API_KEY = "Jcoz6le8AmJeRWYTASZIr5aBE"
-API_SECRET_KEY = "wpSPZGWiAXLPF5xIiTkbFWuQcEY6lXlHEY6ouZ1K1m8rXIRoVd"
-ACCESS_TOKEN = "1263308801622437888-xkA22p1m5fNdH0Ndz4FxgiwB5s7Kuu"
-ACCESS_TOKEN_SECRET = "q3ss2jaI8XRCE3jxJlghFfwduSK04dlmcdupnI62SBzCr"
-CALLBACK_URL = "https://twitter.com/Kai78919017"
+API_KEY = 
+API_SECRET_KEY = 
+ACCESS_TOKEN = 
+ACCESS_TOKEN_SECRET = 
+
 
 
 if __name__ == "__main__":
@@ -15,3 +14,6 @@ if __name__ == "__main__":
     print("API_SECRET_KEY = {}".format(API_SECRET_KEY))
     print("ACCESS_TOKEN = {}".format(ACCESS_TOKEN))
     print("ACCESS_TOKEN_SECRET = {}".format(ACCESS_TOKEN_SECRET))
+
+:wq
+
