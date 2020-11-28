11b5924ac5fb5c419581cbac9c48260b097e3c0e
R0913 <renesmee0420@gmail.com>
2020-11-28 15:22:53 +0900

    一行ずつ品詞抽出

--- free.py
+++ free.py
@@ -3,31 +3,37 @@ import MeCab
 import csv
 import json
 import codecs
-
+import collections
 
 
 # 解析対象テキストファイルを開く
 with open('predicted_data.csv', newline='') as f:
 
-# ファイルを読み込む
-    sample_text = f.read()
+# ファイルを読み込むo
+    sample_text = f.read().split("\n")
 
-
-tokenizer = MeCab.Tagger("-Ochasen")
+tokenizer = MeCab.Tagger('../opt/anaconda3/lib/python3.7/site-packages/Mecab/mecab-ipadic-neologd')
 tokenizer.parse("")
-node = tokenizer.parseToNode(sample_text)
-keywords = []
-for linr in sample_text:
+
+
+for line in sample_text:
+    keywords = []
+    node = tokenizer.parseToNode(line)
 
     while node:
-        if node.feature.split(",")[1] == u"固有名詞":
+        if node.feature.split(",")[1] == "固有名詞":
             keywords.append(node.surface)
-        elif node.feature.split(",")[1] == u"一般名詞":
+        elif node.feature.split(",")[1] == "一般":
             keywords.append(node.surface)
-        elif node.feature.split(",")[0] == u"動詞":
+        elif node.feature.split(",")[0] == "動詞":
             keywords.append(node.feature.split(",")[6])
 
 
         node = node.next
 
-    print(keywords, file=codecs.open(".txt","w"))
+    c = collections.Counter(keywords)
+    print(c)
+    print(keywords, file=codecs.open("count_word.txt","a"))
+
+#, file=codecs.open("count_word.txt","w")
+#  writer = csv.writer(files, lineterminator='\n')
